75cab05a834d089efd233a7426bcdbc9fcfa4859
lervik <axel.lervikgmail.com>
2022-03-07 15:29:37 +0100

    idk anymore

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,15 +9,15 @@ class Home extends React.Component {
                 <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    This is a ... <code>work in progress</code>? 
                 </p>
                 <a
                     className="App-link"
-                    href="https://reactjs.org"
+                    href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    idk
                 </a>
                 </header>
             </div>
